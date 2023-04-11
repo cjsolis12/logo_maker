5f04a07e0744805a9f63b7ad0ac90ba02cf57193
cjsolis <cjelizondo12@gmail.com>
2023-04-11 13:47:42 -0500

    added inquirer prompt to index.js

---
+++ index.js
@@ -0,0 +1,39 @@
+const inquirer = require('inquirer');
+const fs = require('fs')
+const generateShapes = require("./lib/shapes");
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'text',
+            message: 'Enter 3 characters for your logo:',
+        },
+        {
+            type: 'input',
+            name: 'text color',
+            message: 'Please enter your text color:',
+        },
+        {
+            type: "list",
+            name: "shape",
+            message: "Please select the type of shape for your logo:",
+            choices: ["circle", "triangle", "square"],
+          },
+          {
+            type: 'input',
+            name: 'shape color',
+            message: 'Please enter your shape color',
+        },
+    ])
+    .then((answers) => {
+        const svgContent = generateShapes(answers);
+
+        fs.writeFile('logo.svg', svgContent, err =>{
+            if (err){
+                console.log(err);
+                return
+            }
+            console.log('Logo saved as a logo.svg');
+        })
+    })
